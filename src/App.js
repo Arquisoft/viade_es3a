02cc8623192cb164dad611d56af40a777f671c7c
Iván <uo265368@uniovi.es>
2020-03-04 14:49:23 +0100

    WebStorm

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ import {
     LogoutButton
 } from '@inrupt/solid-react-components';
 
-const HeaderWrapper = styled.section `
+const HeaderWrapper = styled.section`
   margin-top: 60px;
   text-align: center;
   width: 100%;
 `;
 
-const DemoWrapper = styled.div `
+const DemoWrapper = styled.div`
   max-width: 900px;
   margin: 0 auto;
 `;
 
-const Headline = styled.h1 `
+const Headline = styled.h1`
   color: #333;
   font-size: 36px;
   font-weight: 300;
@@ -28,15 +28,11 @@ const Headline = styled.h1 `
 ;
 
 const Header = () => {
-    return ( <
-        HeaderWrapper >
-        <
-        img src = { SolidImg }
-        alt = "React logo"
-        width = "62" / >
-        <
-        Headline > VIADE ES3A < /Headline> <
-        /HeaderWrapper>
+    return (
+        <HeaderWrapper>
+            <img src={SolidImg} alt="React logo" width="62" />
+            <Headline>VIADE ES3A</Headline>
+        </HeaderWrapper>
     );
 };
 
@@ -45,12 +41,11 @@ const App = () => {
     const webId = useWebId();
     return (
 
-        <
-        DemoWrapper >
-        <
-        Header / > {
-            webId && ( <
-                ProfileViewer {... {
+        <DemoWrapper>
+            <Header/>
+            {webId && (
+                <ProfileViewer
+                    {...{
                         webId,
                         direction: 'down',
                         viewMoreText: 'See Profile',
@@ -59,25 +54,18 @@ const App = () => {
                             console.log('ERROR', error.statusText);
                         },
                         onClick: true
-                    }
-                } >
-                <
-                button > Hover over me! < /button> <
-                /ProfileViewer>
-            )
-        }
+                    }}
+                >
+                    <button>Hover over me!</button>
+                </ProfileViewer>
+            )}
 
-        <
-        br / >
-        <
-        LoggedOut > < ProviderLogin callbackUri = { `${window.location.origin}/` }
-        /></LoggedOut >
-        <
-        LoggedIn > < LogoutButton / > < /LoggedIn> <
-        /DemoWrapper>
+            <br />
+            <LoggedOut><ProviderLogin callbackUri={`${window.location.origin}/`} /></LoggedOut>
+            <LoggedIn><LogoutButton /></LoggedIn>
+        </DemoWrapper>
     );
 };
-
 
 export default App;
 //<AuthButton popup='https://solid.github.io/solid-auth-client/dist/popup.html' login="Login here!" logout="Log me out"/> Por si fuese necesario
