3f9fe52b60da73fd7f7e119869b8b0f839950590
uo264033 <uo264033@uniovi.es>
2020-03-15 14:48:09 +0100

    Trying to draw a line

--- src/MainPage/Panel/Map/Map.js
+++ src/MainPage/Panel/Map/Map.js
@@ -23,18 +23,52 @@ export default class Map extends React.Component{
             maxNativeZoom:17,
         }).addTo(this.map);
 
+        //Markers --> Inicio, Fin
         var marker2 = L.marker([43.361070,-5.850456,16]).addTo(this.map);
         var marker = L.marker([43.354877,-5.851336,16]).addTo(this.map);
         
         marker2.bindPopup("<b>Campo San Francisco</b><br>Aquí finaliza nuestra ruta").openPopup();
         marker.bindPopup("<b>Escuela Ingeniería Informática</b><br>Aquí empieza nuestra ruta").openPopup();
         
+        //GeoJson --> pintar ruta
+        var geojsonFeature = {
+            
+            "type": "Feature",
+            "properties": {
+                "name": "Coors Field",
+                "amenity": "Baseball Stadium",
+                "popupContent": "This is where the Rockies play!"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [43.305, -5.60]
+            }
+        };
 
-       
+        //Añadimos Capa GeoJson
+        L.geoJSON(geojsonFeature).addTo(this.map);
+
+        //Coordenadas de la ruta para pintar la linea
+        var myLines = [{
+            "type": "LineString",
+            "coordinates": [[43.354877,-5.851336,16], [43.361070,-5.850456,16]]
+        }];
+
+        //Estilo de la linea
+        var myStyle = {
+            "color": "#4169e1",
+            "weight": 5,
+            "opacity": 0.65
+        };
+        
+        L.geoJSON(myLines, {
+            style: myStyle
+        }).addTo(this.map);
     }
 
         render(){
             return <Wrapper width="70vw" height="100vh" id="map"  className="rightPanel_mapa"/>
         }
 
+        
 }
