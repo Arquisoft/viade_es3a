42b36f56978db3ea62a22e8244d288dab008f9ff
uo264033 <uo264033@uniovi.es>
2020-03-25 20:04:52 +0100

    Friends cards without username

--- src/MainPage/Panel/Friends/Friends.js
+++ src/MainPage/Panel/Friends/Friends.js
@@ -2,20 +2,30 @@ import React from 'react'
 import { useWebId,List,Value,evaluateExpressions,evaluateList } from '@solid/react';
 
 
+const Card = () => {
+    return(
+        <div class="card bg-info text-white">
+            <div class="card-body">
+                <h4 class="card-title" id="routeName">Friend name</h4>
+                <br></br>
+                <button type="button" class="btn btn-light">Profile</button>
+            </div>
+        </div> 
+    )
+}
+
 const Friends = () => {
     const webId = useWebId();
     return (
         <div>
             <h2>Your friends, <Value src="user.name" /> </h2>
-            <ul>
-                <p>
                     <List src={`[${webId}].friends`}>{friend =>
                         <li key={friend}>
-                                <Value src={`[${friend}].name`}>{`${friend}`}</Value>
+                            <p>
+                                <Card src={`[${friend}].name`}>{`${friend}`}</Card>
+                                </p>
                         </li>}
                     </List>
-                </p>
-            </ul>
         </div>
     )
 }
