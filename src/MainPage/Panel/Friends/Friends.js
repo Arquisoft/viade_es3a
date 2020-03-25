032ba68cc0a4cdf068497ef01b71c23ebb4e1c30
Andrea López Suárez <uo264703@uniovi.es>
2020-03-25 19:55:32 +0100

    Showing user friends name

--- src/MainPage/Panel/Friends/Friends.js
+++ src/MainPage/Panel/Friends/Friends.js
@@ -1,16 +1,21 @@
 import React from 'react'
-import { useWebId, List, Value } from '@solid/react';
+import { useWebId,List,Value,evaluateExpressions,evaluateList } from '@solid/react';
 
 
 const Friends = () => {
     const webId = useWebId();
     return (
         <div>
-
-            <h2>Your friends, <Value src="user.name"/> </h2> 
-
-            <List src="user.friends" />
-
+            <h2>Your friends, <Value src="user.name" /> </h2>
+            <ul>
+                <p>
+                    <List src={`[${webId}].friends`}>{friend =>
+                        <li key={friend}>
+                                <Value src={`[${friend}].name`}>{`${friend}`}</Value>
+                        </li>}
+                    </List>
+                </p>
+            </ul>
         </div>
     )
 }
