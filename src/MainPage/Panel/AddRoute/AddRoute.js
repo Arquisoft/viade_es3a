4d08f3f0caafe006957c418db65505785b6a66b6
Elena <uo258525@uniovi.es>
2020-04-03 10:25:33 +0200

    Finally able to clear all fields after adding a route

--- src/MainPage/Panel/AddRoute/AddRoute.js
+++ src/MainPage/Panel/AddRoute/AddRoute.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect, useRef } from 'react';
 import './AddRoute.css';
 import { useWebId } from '@solid/react';
 import {
@@ -14,14 +14,15 @@ import { Redirect} from 'react-router-dom';
 
 const fileClien = new fileClient(solidAuth, { enableLogging: true });
 
-const Upload = () => {
-    const [file, setFile] = useState('');
-    const [filename, setFileName] = useState('Choose File');
+const Upload = ({setFile, file}) => {
+    const filename = file == null ? 'Choose File' : file.name;
 
     const changeName = e => {
         setFile(e.target.files[0]);
-        setFileName(e.target.files[0].name);
     }
+    //para acceder a componentes del dum desde react
+    //const refFile = useRef();
+
 
     return (
         <div class="input-group">
@@ -29,7 +30,7 @@ const Upload = () => {
                 <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
             </div>
             <div class="custom-file">
-                <input type="file" class="custom-file-input" id="route"
+                <input value={null} type="file" class="custom-file-input" id="route"
                     aria-describedby="inputGroupFileAddon01" onChange={changeName} required/>
                 <label class="custom-file-label" for="inputGroupFile01"id="labelRoute">{filename}</label>
             </div>
@@ -38,30 +39,37 @@ const Upload = () => {
 };
 const Data = () => {
     var user=""+useWebId();
- 
+    //it saves the actual state of the data
+    const [file, setFile] = useState(null);
+
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState(null);
+    const [video, setVideo] = useState(null);
+
     const url=user.split("profile/card#me")[0]+"/private/routes3a";
     console.log(url);
     return (
         <div>
         
-            <Upload/>
+            <Upload setFile={setFile} file={file}/>
             <div class="form-group">
                 <label for="exampleFormControlInput1">Name:</label>
-                <input type="text" class="form-control" id="name" placeholder="Route's name" required/>
+                <input type="text" class="form-control" id="name" placeholder="Route's name" required value={name} onChange={(e) => setName(e.target.value)}/>
             </div>
             <div class="form-group">
                 <label for="exampleFormControlTextarea1">Description:</label>
-                <textarea class="form-control" id="description" name="description" rows="3" ></textarea>
+                <textarea class="form-control" id="description" name="description" rows="3" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
             </div>
             <div class="form-group">
                 <label class="exampleInputPhoto" for="photo">Imagen:</label><br></br>
-                <input type="file" id="photo" name="image" accept=".png" multiple="true"/>
+                <input value={null} type="file" id="photo" name="image" accept=".png" multiple="true" onChange={(e) => setImage(e.target.files)}/>
             </div>
             <div class="form-group">
                 <label class="exampleInputVideo" for="video">Vídeo:</label><br></br>
-                <input type="file" id="video" name="video" accept=".mp4" multiple="true"/>
+                <input value={null} type="file" id="video" name="video" accept=".mp4" multiple="true" onChange={(e) => setVideo(e.target.files)}/>
             </div>
-            <button onClick={()=> createFolder(url)}  class="btn btn-info" >Add route
+            <button onClick={()=> createFolder(url, file, name, description, image, video, setFile, setName, setDescription, setImage, setVideo)}  class="btn btn-info" >Add route
             </button>
             
             
@@ -84,8 +92,8 @@ const AddRoute = () => {
 
 
 
-const createFolder = async (folder) => {
-    if(document.getElementById("name").value===""||document.getElementById("route").files[0]==null){
+const createFolder = async (folder, route, name, description, photo, video,setFile, setName, setDescription, setImage, setVideo) => {
+    if (name===""||route==null){
         alert("Name or route is empty!");
     }
     else{
@@ -93,19 +101,19 @@ const createFolder = async (folder) => {
     if (!existe)
         await fileClien.createFolder(folder);
     var fileList = [];
-    var nameValue = document.getElementById("name").value;
+    var nameValue = name;
     var destination = folder + "/" + nameValue + "/";
     existe = await fileClien.itemExists(destination);
     if (!existe) {
         var k=0;
         await fileClien.createFolder(destination);
-        fileList.push(document.getElementById("route").files[0]);
-        await fileClien.createFile(destination + "/"+ "description", document.getElementById("description").value, "text/plain");
-        for(k=0; k<document.getElementById("photo").files.length; k++){
-            await fileClien.createFile(destination + "/"+ "photo" + "/img" + (k+1), document.getElementById("photo").files[k], "img");
+        fileList.push(route);
+        await fileClien.createFile(destination + "/"+ "description", description, "text/plain");
+        for(k=0; photo != null && k<photo.length; k++){
+            await fileClien.createFile(destination + "/"+ "photo" + "/img" + (k+1), photo[k], "img");
         }
-        for(k=0; k<document.getElementById("video").files.length; k++){
-            await fileClien.createFile(destination + "/"+ "video"+ "/vid" + (k+1), document.getElementById("video").files[k], "video");
+        for(k=0; video != null && k<video.length; k++){
+            await fileClien.createFile(destination + "/"+ "video"+ "/vid" + (k+1), video[k], "video");
         }
 
         for (var i = 0; i < fileList.length; i++) {
@@ -115,10 +123,15 @@ const createFolder = async (folder) => {
         }
         alert("Your route has been added to the pod!!");
         //clean all fields
-        document.getElementById("name").value="";
-        document.getElementById("description").value="";
-        document.getElementById("route").value=null;
-        document.getElementById("labelRoute").value=null;
+        setName('');
+        setDescription('');
+        setFile(null);
+        setImage(null);
+        setVideo(null);  
+        
+        document.getElementById('photo').value=null;
+        document.getElementById('video').value=null;
+        document.getElementById('route').value=null;
     }
     else
         alert("Route title already used, use another title");
