f3b54c5f6f1b01c08196be012a33070342321e84
Andrea López Suárez <uo264703@uniovi.es>
2020-04-07 13:19:09 +0200

    Add profile photo

--- src/MainPage/Panel/Profile/Profile.js
+++ src/MainPage/Panel/Profile/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useWebId, ProfileViewer } from '@inrupt/solid-react-components';
 import { Value, Link } from '@solid/react';
-import { ReactComponent as ReactLogo } from '../../../react.svg';
+import { ReactComponent as ProfileLogo } from '../../../img/profile.svg';
 import './Profile.css'
 import Avatar from '@material-ui/core/Avatar';
 import FaceIcon from '@material-ui/icons/Face';
@@ -11,35 +11,35 @@ const Profile = () => {
     const webId = useWebId();
     return (
         <div>
-        <p><h2 class="h2">Profile</h2></p>
-        <div className="card">
-            <div className="photo">
-            {webId && (
-                <ProfileViewer
-                    {...{
-                        webId,
-                        direction: 'down',
-                        onError: error => {
-                            // eslint-disable-next-line no-console
-                            console.log('ERROR', error.statusText);
-                        },
-                        onClick: false
-                    }} 
-                    >
-                         <Avatar alt={"user.name"}><FaceIcon/></Avatar>
-                    </ProfileViewer>
-            )}
-            </div>
-            <div className="info">
-                <div className="name">
-                    <Value src="user.name"/>
+            <p><h2 class="h2">Profile</h2></p>
+            <div className="card">
+                <div className="photo">
+                    {webId && (
+                        <ProfileViewer
+                            {...{
+                                webId,
+                                direction: 'down',
+                                onError: error => {
+                                    // eslint-disable-next-line no-console
+                                    console.log('ERROR', error.statusText);
+                                },
+                                onClick: false
+                            }}
+                        >
+                            <ProfileLogo height="16vh" width="16vh" className="icon" />
+                        </ProfileViewer>
+                    )}
                 </div>
-                <div className="user">Viade's user</div>
-                <div className="divider"></div>
-                <Link href={`[${webId}]`} className="buttonSolid">Go to SOLID profile</Link>
-            </div>
+                <div className="info">
+                    <div className="name">
+                        <Value src="user.name" />
+                    </div>
+                    <div className="user">Viade's user</div>
+                    <div className="divider"></div>
+                    <Link href={`[${webId}]`} className="buttonSolid">Go to SOLID profile</Link>
+                </div>
 
-        </div>
+            </div>
         </div>
     )
 }
