c3266fd0d8a1b9ce920312d1c62f39bb9e0e8e45
uo264033 <uo264033@uniovi.es>
2020-04-08 13:22:18 +0200

    my friends and my routes added to Profile part I

--- src/MainPage/Panel/Profile/Profile.js
+++ src/MainPage/Panel/Profile/Profile.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { useWebId, ProfileViewer } from '@inrupt/solid-react-components';
-import { Value, Link } from '@solid/react';
+import { Value, Link, List } from '@solid/react';
 import { ReactComponent as ProfileLogo } from '../../../img/profile.svg';
 import './Profile.css'
 
 import DocumentTitle from "react-document-title";
 
 
+const Data = (props) => {
+    return  (
+        props.lenght
+    )
+}
+
 const Profile = () => {
     const webId = useWebId();
+    // const friendsCount =; 
+    // const routesCount =;
     return (
         <DocumentTitle title='Profile'>
             <div>
@@ -41,7 +49,33 @@ const Profile = () => {
                     </div>
 
                 </div>
+                <br></br>
+                
+                <div className="card-friends">
+                    <div className="info">
+                        <div className="friends"> My friends</div>
+                        <div className="data">6</div>                        
+                        {/* <List src={`[${webId}].friends`} className="data">{
+                            friends => friends.lenght
+                        }
+                        </List> */}
+                        <div ></div>
+                             {/* /viade_es3a/friends */}
+                            <Link href={`[${webId}]`} className="buttonFriends">Show friends</Link>
+                    </div>                
+                </div>
+                <div className="card-routes">
+                    <div className="info">
+                        <div className="routes"> My routes</div>
+                        <div className="data">5</div>
+                        <div ></div>
+                            {/* /viade_es3a/loadRoute */}
+                            <Link href={`[${webId}]`} className="buttonFriends">Show route</Link>
+                    </div>                
+                </div>
+                
             </div>
+           
         </DocumentTitle>
     )
 }
