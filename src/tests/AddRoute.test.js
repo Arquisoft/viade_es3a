7db74fbaf54c83984f8d3e423a26814101a4a647
Iván <uo265368@uniovi.es>
2020-04-09 20:50:09 +0200

    Elements addRoute are present

--- src/tests/AddRoute.test.js
+++ src/tests/AddRoute.test.js
@@ -7,3 +7,12 @@ test('Not crashing component', ()=>{
     const div = document.createElement('div');
     ReactDOM.render(<AddRoute></AddRoute>,div);
 })
+
+test('Elements addRoute are present', () => {
+    const { getByTestId } = render(<AddRoute></AddRoute>);
+    expect(getByTestId("route")).toBeInTheDocument();
+    expect(getByTestId("name")).toBeInTheDocument();
+    expect(getByTestId("desc")).toBeInTheDocument();
+    expect(getByTestId("img")).toBeInTheDocument();
+    expect(getByTestId("vid")).toBeInTheDocument();
+  });
