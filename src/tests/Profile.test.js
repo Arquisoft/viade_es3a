698dad660c1547c3d3c0f480f1493fc8d8299f53
Andrea López Suárez <uo264703@uniovi.es>
2020-04-10 13:24:34 +0200

    Profile elements are present test

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import ReactDOM from 'react-dom';
 import Profile from '../MainPage/Panel/Profile/Profile';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 test('Not crashing component', () => {
     const div = document.createElement('div');
     ReactDOM.render(
         <Router><Profile></Profile></Router>, div);
-})
+});
+
+test('Profile elements are present', () => {
+    const { getByTestId } = render(<Router><Profile></Profile></Router>);
+    expect(getByTestId("titleProfile")).toBeInTheDocument();
+    expect(getByTestId("photoProfile")).toBeInTheDocument();
+    // expect(getByTestId("viewerProfile")).toBeInTheDocument();
+    // expect(getByTestId("logoProfile")).toBeInTheDocument();
+    expect(getByTestId("userProfile")).toBeInTheDocument();
+    expect(getByTestId("dividerProfile")).toBeInTheDocument();
+    expect(screen.getByText("Go to SOLID profile")).toBeInTheDocument()
+    expect(getByTestId("myFriendsProfile")).toBeInTheDocument();
+    expect(getByTestId("friendsLengthProfile")).toBeInTheDocument();
+    expect(getByTestId("buttonFriendsProfile")).toBeInTheDocument();
+    expect(getByTestId("myRoutesProfile")).toBeInTheDocument();
+    expect(getByTestId("routesLengthProfile")).toBeInTheDocument();
+    expect(getByTestId("buttonRoutesProfile")).toBeInTheDocument();
+});
