96f34ae6701c92314f8bb9635e54c515f8562dcf
Andrea López Suárez <uo264703@uniovi.es>
2020-04-11 19:20:22 +0200

    First changes (now it's horrible and ugly)

--- src/MainPage/NavBar/NavBar.js
+++ src/MainPage/NavBar/NavBar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './NavBar.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import {
-  LogoutButton
-} from '@inrupt/solid-react-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, NavItem, Grid, Row, Col } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { LogoutButton } from '@inrupt/solid-react-components';
+
+// Iconos
 import BackupOutlinedIcon from '@material-ui/icons/BackupOutlined';
 import RoomOutlinedIcon from '@material-ui/icons/RoomOutlined';
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
@@ -12,59 +13,50 @@ import PeopleAltOutlinedIcon from '@material-ui/icons/PeopleAltOutlined';
 import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
 import ShareIcon from '@material-ui/icons/Share';
 
+
 const NavBar = () => {
   // const webId = useWebId();
   return (
-    
-    <header className="navBar">
-      <nav className="navbar navbar-dark bg-dark" display="flex" position="fixed">
-        <div className="navBarLogo">
-         
-          <Link to="/viade_es3a" data-testid="logo">
-            <img src={require('../../img/logo_viade_es3a.jpg')} alt="React logo" width="160" />
-          </Link>
-          
-        </div>
-
-        
-        <ul className="navBarOptions">
-          <li className="option">
+    <Navbar bg="dark" variant="dark" expand="lg" fixed="top" className="navBar">
+      <Navbar.Brand as={Link} to="/viade_es3a" className="logo">
+        <img src={require('../../img/logo_viade_es3a.jpg')} alt="React logo" width="122" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <NavItem href="/viade_es3a/addRoute" className="opcionYLogo">
             <BackupOutlinedIcon className="icon" />
-            <Link to="/viade_es3a/addRoute" data-testid="add"> Add route </Link>
-          </li>
-          <li className="option">
+            <Nav.Link as={Link} to="/viade_es3a/addRoute" className="option">Add Route</Nav.Link>
+          </NavItem>
+          <NavItem href="/viade_es3a/loadRoute" bg="light" className="opcionYLogo">
             <RoomOutlinedIcon className="icon" />
-            <Link to="/viade_es3a/loadRoute" data-testid="load" >My routes</Link>
-          </li>
-          <li className="option">
+            <Nav.Link as={Link} to="/viade_es3a/loadRoute" className="option">My routes</Nav.Link>
+          </NavItem>
+          <NavItem href="/viade_es3a/shareRoute" className="opcionYLogo">
             <ShareIcon className="icon" />
-            <Link to="/viade_es3a/shareRoute" data-testid="share">Share routes</Link>
-          </li>
-          <li className="option">
+            <Nav.Link as={Link} to="/viade_es3a/shareRoute" className="option">Share routes</Nav.Link>
+          </NavItem>
+          <NavItem href="/viade_es3a/SharedRoutes" className="opcionYLogo">
             <PeopleAltOutlinedIcon className="icon" />
-            <Link to="/viade_es3a/SharedRoutes" data-testid="friendsRoutes"> Friend's Routes </Link>
-          </li>
-          <li className="option">
+            <Nav.Link as={Link} to="/viade_es3a/SharedRoutes" className="option">Friend's Routes</Nav.Link>
+          </NavItem>
+          <NavItem href="/viade_es3a/profile" className="opcionYLogo">
             <AccountCircleOutlinedIcon className="icon" />
-            <Link to="/viade_es3a/profile" data-testid="profile"> Profile </Link>
-          </li>
-          <li className="option">
+            <Nav.Link as={Link} to="/viade_es3a/profile" className="option">Profile</Nav.Link>
+          </NavItem>
+          <NavItem href="/viade_es3a/friends" className="opcionYLogo">
             <PeopleAltOutlinedIcon className="icon" />
-            <Link to="/viade_es3a/friends" data-testid="friends"> Friends </Link>
-          </li>
-
-        </ul>
-        <div className="navBarLogout">
-         
-          <li>
-            <ExitToAppOutlinedIcon className="icon" />
-            <LogoutButton data-testid="logout"/>
-          </li>
-          
-        </div>
-      </nav>
-    </header>
-    
+            <Nav.Link as={Link} to="/viade_es3a/friends" className="option">Friends</Nav.Link>
+          </NavItem>
+        </Nav>
+        <Nav pullRight>
+          <NavItem href="/viade_es3a/logout">
+          <ExitToAppOutlinedIcon className="icon" />
+            <LogoutButton data-testid="logout" className="logout" />
+          </NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
