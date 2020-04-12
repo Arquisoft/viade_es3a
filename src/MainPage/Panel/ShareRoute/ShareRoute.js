d8027af5a0d1a3f090a8419e196a3571479cf1bf
christiangrs <christiangrs1998@gmail.com>
2020-04-12 06:01:54 +0200

    ya

--- src/MainPage/Panel/ShareRoute/ShareRoute.js
+++ src/MainPage/Panel/ShareRoute/ShareRoute.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { List, useWebId,Value, Name, Link  } from "@solid/react";
+import { List, useWebId, Value, Name, Link } from "@solid/react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./ShareRoute.css";
 import * as solidAuth from "solid-auth-client";
@@ -7,18 +7,10 @@ import fileClient from "solid-file-client";
 
 import * as algo from "../Map/Map";
 
-const SolidAclUtils = require("solid-acl-utils");
 const auth = require("solid-auth-client");
-const { AclApi, AclDoc, AclParser, AclRule, Permissions, Agents } = SolidAclUtils;
-const { READ, WRITE, APPEND, CONTROL } = Permissions;
-
-const fetch = auth.fetch.bind(auth);
-
-const aclApi = new AclApi(fetch, { autoSave: true });
-
 
 const fileClien = new fileClient(solidAuth, { enableLogging: true });
-var urlRutas=[];
+var urlRutas = [];
 const LoadRoute = () => {
 
     const [folders, setFolders] = useState([]);
@@ -32,93 +24,93 @@ const LoadRoute = () => {
 
     console.log(selected);
 
-    var user=useWebId();
+    var user = useWebId();
     const webId = useWebId();
     var coll = document.getElementsByClassName("collapsible");
     var i;
 
     for (i = 0; i < coll.length; i++) {
-         coll[i].addEventListener("click", function() {
-         this.classList.toggle("active");
-         var content = this.nextElementSibling;
-          if (content.style.display === "block") {
-              content.style.display = "none";
-         } else {
-              content.style.display = "block";
-          }
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
         });
     }
     useEffect(() => {
-        if ( user != undefined) {
-            const url=user.split("profile/card#me")[0]+"/private/routes3a";
+        if (user != undefined) {
+            const url = user.split("profile/card#me")[0] + "/private/routes3a";
             //listRoutes(url);
             loadRoutes(url, setFolders);
         }
     }, [user]);
-    
-        return (
-            <div  class="container">
-                <h2 data-testid="label" id="rutas" class="h2">Share a route with your friends:</h2>
 
-                <ul>
+    return (
+        <div class="container">
+            <h2 data-testid="label" id="rutas" class="h2">Share a route with your friends:</h2>
+
+            <ul>
                 {
-                    folders.map((folder,i) => {
-                        var urlArchivo= ""+folder.url;
-                        var arrayUrl=urlArchivo.split("/");
+                    folders.map((folder, i) => {
+                        var urlArchivo = "" + folder.url;
+                        var arrayUrl = urlArchivo.split("/");
                         urlRutas.push(urlArchivo);
-                        var nombre=arrayUrl[arrayUrl.length-2].split("%20").join(" ");
+                        var nombre = arrayUrl[arrayUrl.length - 2].split("%20").join(" ");
                         return (
-                        <li key={"folder_"+i}>
-                            <a href="#" class={"lista"} onClick={() => loadRoute(urlArchivo, setSelected)}>
-                                {nombre}
-                            </a>
-                        </li>);
+                            <li key={"folder_" + i}>
+                                <a href="#" class={"lista"} onClick={() => loadRoute(urlArchivo, setSelected)}>
+                                    {nombre}
+                                </a>
+                            </li>);
                     })
                 }
-                </ul>
-                <div data-testid="card" class="card bg-info text-white">
-                    <div class="card-body">
-                        <h4 class="card-title" id="routeName">{selected.name}</h4>
-                        <p class="card-Description" id ="routeDescription">{selected.description}</p>
-                        <div className="card-Image" id="routeImage">
-                            {
-                                selected.images.map((image,i) => (
-                                    <div key={"image_"+i}><img src={image} class={"imag"}/></div>
-                                ))
-                            }
-                        </div>
-                        <div id="ImgDiv"><div id="images"></div></div><br></br>
-                        <div className="card-Video" id="routeVideo">
+            </ul>
+            <div data-testid="card" class="card bg-info text-white">
+                <div class="card-body">
+                    <h4 class="card-title" id="routeName">{selected.name}</h4>
+                    <p class="card-Description" id="routeDescription">{selected.description}</p>
+                    <div className="card-Image" id="routeImage">
                         {
-                                selected.videos.map((video,i) => (
-                                    <div key={"video_"+i}><video src={video} class={"vid"} controls/></div>
-                                ))
-                            }
-                        </div>
-                        <div id="VidDiv"><div id="videos"></div></div><br></br>
-                        <center>
-                            <h2 className="h2">Share with a friend:  </h2>
-                        <List  src={`[${user}].friends`} className="list" padding-inline-start="0">{(friend) =>
-                            <li key={friend} className="listElement">
-                            <p>
-                                <Carda nombre={`[${friend}]`} url={selected.url} name={selected.name}></Carda>
-                            </p>
-                            </li>}
-                    </List>
-
-                    
-
-  
-                        </center>
-                        
-                    
-                        
-                        
+                            selected.images.map((image, i) => (
+                                <div key={"image_" + i}><img src={image} class={"imag"} /></div>
+                            ))
+                        }
                     </div>
-                </div> 
+                    <div id="ImgDiv"><div id="images"></div></div><br></br>
+                    <div className="card-Video" id="routeVideo">
+                        {
+                            selected.videos.map((video, i) => (
+                                <div key={"video_" + i}><video src={video} class={"vid"} controls /></div>
+                            ))
+                        }
+                    </div>
+                    <div id="VidDiv"><div id="videos"></div></div><br></br>
+                    <center>
+                        <h2 className="h2">Share with a friend:  </h2>
+                        <List src={`[${user}].friends`} className="list" padding-inline-start="0">{(friend) =>
+                            <li key={friend} className="listElement">
+                                <p>
+                                    <Carda nombre={`[${friend}]`} url={selected.url} name={selected.name}></Carda>
+                                </p>
+                            </li>}
+                        </List>
+
+
+
+
+                    </center>
+
+
+
+
+                </div>
             </div>
-           
-        );
+        </div>
+
+    );
 };
 
 async function loadRoutes(url, setFolders) {
@@ -128,7 +120,7 @@ async function loadRoutes(url, setFolders) {
 }
 
 async function loadRoute(urlCarptetaRuta, setSelected) {
-    
+
     let folder = await fileClien.readFolder(urlCarptetaRuta);
     let folderDesc = await fileClien.readFile(urlCarptetaRuta + "description");
     let images = await loadFile(urlCarptetaRuta, "photo/img");
@@ -146,76 +138,82 @@ async function loadRoute(urlCarptetaRuta, setSelected) {
 
 }
 
-async function loadFile(urlCarptetaRuta, route){
+async function loadFile(urlCarptetaRuta, route) {
     var k;
     var result = [];
-    for(k=0; k<1000; k++){
-        try{
-            await fileClien.readFile(urlCarptetaRuta + route + (k+1));
-            result.push(urlCarptetaRuta + route + (k+1));
-        }catch{
-            k=1000;
+    for (k = 0; k < 1000; k++) {
+        try {
+            await fileClien.readFile(urlCarptetaRuta + route + (k + 1));
+            result.push(urlCarptetaRuta + route + (k + 1));
+        } catch{
+            k = 1000;
         }
     }
     return result;
 }
 
 export async function showRoute(urlCarptetaRuta) {
-    
+
     let folder = await fileClien.readFolder(urlCarptetaRuta);
 
-    
+
     document.getElementById("routeName").innerHTML = (folder.name).split("%20").join(" ");
-    let ruta = await fileClien.readFile(urlCarptetaRuta+folder.name+".geojson");
+    let ruta = await fileClien.readFile(urlCarptetaRuta + folder.name + ".geojson");
 
     algo.updateMap(ruta, folder.name);
-    
-}
-async function enseñaAmigos(source,target,name){
-   
-    const target2=target.split("[")[1];
-    const urlTarget=target2.split("profile/card#me")[0]+"/inbox/routes3a";
 
-    let acl = await fileClien.readFile(source+"/.acl");
+}
+async function enseñaAmigos(source, target, name) {
+
+    const target2 = target.split("[")[1];
+    const urlTarget = target2.split("profile/card#me")[0] + "/inbox/routes3a";
+
+    let acl = await fileClien.readFile(source + "/.acl");
 
     console.log(acl)
     let content = "";
 
-    let userToAcl=target2.split("]")[0]
+    let userToAcl = target2.split("]")[0]
 
+    if (!(acl + "").includes(userToAcl.substring(0, userToAcl.length - 2))) {
+        if (!(acl + "").includes("c0")) {
+            content = "@prefix : <#>.\n" +
+                "@prefix n0: <http://www.w3.org/ns/auth/acl#>.\n" +
+                "@prefix M: <./>.\n" +
+                "@prefix c: </profile/card#>.\n" +
+                "@prefix c0: <" + userToAcl.substring(0, userToAcl.length - 2) + ">.\n" +
+                ":ControlReadWrite\n" +
+                "a n0:Authorization;\n" +
+                "n0:accessTo M:;\n" +
+                "n0:agent c:me;\n" +
+                "n0:default M:;\n" +
+                "n0:mode n0:Control, n0:Read, n0:Write.\n" +
+                ":Read a n0:Authorization; \n" +
+                "n0:accessTo M:; \n" +
+                "n0:agent c0:me;\n" +
+                "n0:default M:; \n" +
+                "n0:mode n0:Read."
+            await fileClien.createFile(source + "/.acl", content, "text/turtle");
+        }
+        else {
 
-    if(!(acl+"").includes("c0")) {
-        console.log("achusma")
-        content = "@prefix : <#>.\n"+
-        "@prefix n0: <http://www.w3.org/ns/auth/acl#>.\n"+
-        "@prefix M: <./>.\n"+
-        "@prefix c: </profile/card#>.\n"+
-        "@prefix c0: <"+userToAcl.substring(0, userToAcl.length-2)+">.\n"+
-        ":ControlReadWrite\n"+
-            "a n0:Authorization;\n"+
-            "n0:accessTo M:;\n"+
-            "n0:agent c:me;\n"+
-            "n0:default M:;\n"+
-            "n0:mode n0:Control, n0:Read, n0:Write.\n"+
-        ":Read a n0:Authorization; \n"+
-        "n0:accessTo M:; \n"+
-        "n0:agent c0:me;\n"+
-        "n0:default M:; \n"+
-        "n0:mode n0:Read."
+            var cont = 1;
+            let nuevo = "";
+            let nuevo2 = "";
+            while ((acl + "").includes("c" + cont)) {
+                cont++;
+            }
+            nuevo = acl.split(":ControlR")[0] + "@prefix c" + cont + ": <" + userToAcl.substring(0, userToAcl.length - 2) + ">.\n" + ":ControlR" + acl.split(":ControlR")[1];
+            nuevo2 = nuevo.split("c0:me")[0] + "c0:me, c" + cont + ":me" + nuevo.split("c0:me")[1];
+            await fileClien.createFile(source + "/.acl", nuevo2, "text/turtle");
+
+        }
+        await fileClien.postFile(urlTarget + "/" + name, source, "text/plain");
+
+        alert("Your route has been shared!");
     }
-
-
-    
-
-    await fileClien.createFile(source+"/.acl", content,"text/turtle");
-                //const aclApi = new AclApi(fetch, { autoSave: true })
-                //const acl = await aclApi.loadFromFileUrl(source)
-
-                //await acl.addRule(READ, target2.split("]")[0])
-
-    //await fileClien.postFile(urlTarget + "/"+ name, source , "text/plain");
-    
-    alert("Your route has been shared!");
+    else
+        alert("Your route was already shared with this person!");
 }
 const Carda = (props) => {
     return (
@@ -224,7 +222,7 @@ const Carda = (props) => {
                 <h4 class="card-title" id="friendName">
                     <Name src={props.nombre}>{props.nombre}</Name>
                 </h4>
-                    <button  className="btn btn-light" onClick={() => enseñaAmigos(props.url,props.nombre,props.name)}>Share</button>
+                <button className="btn btn-light" onClick={() => enseñaAmigos(props.url, props.nombre, props.name)}>Share</button>
             </div>
         </div>
     );
