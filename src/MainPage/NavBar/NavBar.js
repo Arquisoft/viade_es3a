39f7cc4f9fa0a739f3efec56fcd44c82e67a848a
Elena <uo258525@uniovi.es>
2020-04-13 10:41:33 +0200

    better to the left :')

--- src/MainPage/NavBar/NavBar.js
+++ src/MainPage/NavBar/NavBar.js
@@ -52,14 +52,15 @@ const NavBar = () => {
           
         </Nav>
         <Nav pullRight>
+        <NavItem href="/viade_es3a/notifications" className="opcionYLogo">
+            <NotificationsActiveOutlinedIcon className="icon" />
+            <Nav.Link as={Link} to="/viade_es3a/notifications" className="notifications" data-testid="notifications"></Nav.Link>
+          </NavItem>
           <NavItem href="/viade_es3a/logout" className="opcionYLogo">
             <ExitToAppOutlinedIcon className="icon" />
             <LogoutButton data-testid="logout" className="logout" data-testid="logout" />
           </NavItem>
-          <NavItem href="/viade_es3a/notifications" className="opcionYLogo">
-            <NotificationsActiveOutlinedIcon className="icon" />
-            <Nav.Link as={Link} to="/viade_es3a/notifications" className="notifications" data-testid="notifications"></Nav.Link>
-          </NavItem>
+          
         </Nav>
       </Navbar.Collapse>
     </Navbar>
