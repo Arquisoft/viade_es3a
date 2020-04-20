4ed1e20026bec3a146c8e09b6958c6cdd569a082
uo264033 <uo264033@uniovi.es>
2020-04-20 09:51:41 +0200

    Create a route interface structure

--- src/MainPage/Panel/CreateRoute/CreateRoute.js
+++ src/MainPage/Panel/CreateRoute/CreateRoute.js
@@ -7,13 +7,30 @@ const CreateRoute = () => {
         
         <DocumentTitle title="Create route">
             <div>
-                <h2 className="h2">Create a new route</h2>
-                <p>Click on the map to mark different point and be able to do a new route. 
-                    Once you have it, click on 'Save' to save it on your POD</p>
+                <h2 class="h2">Create a route</h2>
+                <br></br>
+                <div class="form-group">
+                    <label for="exampleFormControlInput1" class="labelName" data-testid="name">Name:</label>
+                    <input type="text" class="form-control" id="name" data-testid="inputName" placeholder="Route's name" />
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea1" class="labelDescription" data-testid="desc">Description:</label>
+                    <textarea class="form-control" id="description" data-testid="inputDesc" name="description" rows="3"  ></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label class="exampleInputPhoto" for="photo" class="labelPhoto" data-testid="img">Images:</label><br></br>
+                    <input value={null} type="file" id="photo" name="image" data-testid="inputImg" accept="image/*" multiple="true" />
+                </div>
+                <div class="form-group">
+                    <label class="exampleInputVideo" for="video" class="labelVideo" data-testid="vid">Video:</label><br></br>
+                    <input value={null} type="file" id="video" name="video" accept="video/*" data-testid="inputVid" multiple="true" />
+                </div>
+                <br></br>
                 <center>
-                    <button class="btn btn-info" >Save
-                    </button>
-                </center>
+                <button data-testid="btnenviar"  class="btn btn-info" >Save
+                </button>
+            </center>
             </div>
         </DocumentTitle>
     
