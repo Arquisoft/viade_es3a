209b26d059039ff05761e2249bc409fddaec1e0f
Andrea López Suárez <uo264703@uniovi.es>
2020-04-27 19:03:27 +0200

    Change searchBar style

--- src/MainPage/Panel/Friends/Friends.js
+++ src/MainPage/Panel/Friends/Friends.js
@@ -4,13 +4,13 @@ import "./Friends.css";
 import { Form } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.css";
 import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import DocumentTitle from "react-document-title";
 
 import auth from "solid-auth-client";
 
 const Card = (props, webId) => {
-    var user= ""+ useWebId();
+    var user = "" + useWebId();
     return (
         <div class="card bg-info text-white" >
             <div class="card-body">
@@ -35,24 +35,15 @@ const Friends = () => {
         <DocumentTitle title="Friends">
             <div className="prueba">
                 <h2 className="h2" data-testId="label">Your friends, <Value src="user.name" /> </h2>
-            
-                <div class="card bg-info text-white" >
-                    <div class="card-body">
-                        <h4 class="card-title" id="friendName">
-                            Agregar amigos
-                        </h4>
-                        <center>
-                            <div class="form-group">
-                                <Form.Control type="text" placeholder="https://uo264033.solid.community/profile/card#me" name="webid"/>
-                                <div className="right">
-                                    <IconButton aria-label="search"> Search  
-                                    {/* var friendWebId = getDocumentId("webid"); */}
-                                    {/* onClick={() => addFriend(friendWebId, userWebId)} */}
-                                        <SearchIcon fontSize="large"/>
-                                    </IconButton>
-                                </div>
-                            </div>
-                        </center>
+
+                <h4 class="card-title" id="friendName">Agregar amigos</h4>
+
+                <div class="wrap">
+                    <div class="search">
+                        <input type="text" class="searchTerm" placeholder="https://uo264033.solid.community/profile/card#me" />
+                        <button type="submit" class="searchButton">
+                            <SearchOutlinedIcon className="iconSearch"/>
+                        </button>
                     </div>
                 </div>
 
@@ -60,7 +51,7 @@ const Friends = () => {
                 <List src={`[${webId}].friends`} className="list" padding-inline-start="0">{(friend) =>
                     <li key={friend} className="listElement">
                         <p>
-                            <Card nombre={`[${friend}]`} web= {webId}></Card>
+                            <Card nombre={`[${friend}]`} web={webId}></Card>
                         </p>
                     </li>}
                 </List>
@@ -72,23 +63,23 @@ const Friends = () => {
 
 const deleteFriend = async (friendWebId, userWebId) => {
     const auth = require("solid-auth-client");
-    var friends =  `[${userWebId}].friends`;
-    for(let i=0; i< friends.length; i++){
-        if(friends[i] === friendWebId)
+    var friends = `[${userWebId}].friends`;
+    for (let i = 0; i < friends.length; i++) {
+        if (friends[i] === friendWebId)
             friends.splice(i);
     }
     //Mirar si se elimina en Solid
-  }
+}
 
-  const addFriend =  async (friendWebId, userWebId) => {
+const addFriend = async (friendWebId, userWebId) => {
     const auth = require("solid-auth-client");
-    var friends =  `[${userWebId}].friends`;
-    for(let i=0; i< friends.length; i++){
-        if(friends[i] === friendWebId)
+    var friends = `[${userWebId}].friends`;
+    for (let i = 0; i < friends.length; i++) {
+        if (friends[i] === friendWebId)
             friends.push(i);
     }
     //Mirar si se añade en Solid
-  }
+}
 
 
 export default Friends;
