158d39e83ac1a106cdc6846a876815ae6dd16591
Andrea López Suárez <uo264703@uniovi.es>
2020-05-01 16:55:28 +0200

    Fix Welcome.test

--- src/MainPage/Panel/Welcome/Welcome.js
+++ src/MainPage/Panel/Welcome/Welcome.js
@@ -44,105 +44,105 @@ const AccordionWelcome = () => {
 
     return (
         <Accordion>
-            <Card className="cardWelcome" data-testid="add">
+            <Card className="cardWelcome">
                 <CustomToggle eventKey={1}>
                     <BackupOutlinedIcon className="iconWelcome" data-testid="iconAddRouteWelcome" />
-                    <div className="optionTitle">Add route</div>
+                    <div className="optionTitle" data-testid="add">Add route</div>
                 </CustomToggle>
                 <Accordion.Collapse eventKey={1} className="mostrar">
-                    <Card.Body>You can upload a route and it will be saved in your SOLID pod</Card.Body>
+                    <Card.Body data-testid="addRouteInfoWelcome">You can upload a route and it will be saved in your SOLID pod</Card.Body>
                 </Accordion.Collapse>
             </Card>
 
-            <Card className="cardWelcome" data-testid="routes">
+            <Card className="cardWelcome">
                 <CustomToggle eventKey={2}>
                     <MapOutlinedIcon className="iconWelcome" data-testid="iconRoutesWelcome" />
-                    <div className="optionTitle">Routes</div>
+                    <div className="optionTitle" data-testid="routes">Routes</div>
                 </CustomToggle>
                 <Accordion.Collapse eventKey={2} className="mostrar">
                     <Card.Body>
 
-                        <Card className="cardWelcome" data-testid="load">
+                        <Card className="cardWelcome">
                             <CustomToggle eventKey={2}>
                                 <RoomOutlinedIcon className="iconWelcome" data-testid="iconMyRoutesWelcome" />
-                                <div className="optionTitle">My routes</div>
+                                <div className="optionTitle" data-testid="load">My routes</div>
                             </CustomToggle>
                             <Accordion.Collapse eventKey={2} className="mostrar">
-                                <Card.Body>You can choose one route and show it in the map</Card.Body>
+                                <Card.Body data-testid="loadRouteInfoWelcome">You can choose one route and show it in the map</Card.Body>
                             </Accordion.Collapse>
                         </Card>
 
-                        <Card className="cardWelcome" data-testid="create">
+                        <Card className="cardWelcome">
                             <CustomToggle eventKey={2}>
                                 <CreateOutlinedIcon className="iconWelcome" data-testid="iconCreateRouteWelcome" />
-                                <div className="optionTitle">Create route</div>
+                                <div className="optionTitle" data-testid="create">Create route</div>
                             </CustomToggle>
                             <Accordion.Collapse eventKey={2} className="mostrar">
-                                <Card.Body>You can create your own route and save it</Card.Body>
+                                <Card.Body data-testid="createRouteInfoWelcome">You can create your own route and save it</Card.Body>
                             </Accordion.Collapse>
                         </Card>
                     </Card.Body>
                 </Accordion.Collapse>
             </Card>
 
-            <Card className="cardWelcome" data-testid="share">
+            <Card className="cardWelcome">
                 <CustomToggle eventKey={3}>
                     <ShareIcon className="iconWelcome" data-testid="iconShareRoutesWelcome" />
-                    <div className="optionTitle">Share routes</div>
+                    <div className="optionTitle" data-testid="share">Share routes</div>
                 </CustomToggle>
                 <Accordion.Collapse eventKey={3} className="mostrar">
-                    <Card.Body>You can share your routes with your friends</Card.Body>
+                    <Card.Body data-testid="shareRouteInfoWelcome">You can share your routes with your friends</Card.Body>
                 </Accordion.Collapse>
             </Card>
 
-            <Card className="cardWelcome" data-testid="profile">
+            <Card className="cardWelcome">
                 <CustomToggle eventKey={4}>
                     <AccountCircleOutlinedIcon className="iconWelcome" data-testid="iconProfileWelcome" />
-                    <div className="optionTitle">Profile</div>
+                    <div className="optionTitle" data-testid="profile">Profile</div>
                 </CustomToggle>
                 <Accordion.Collapse eventKey={4} className="mostrar">
-                    <Card.Body>Your personal data</Card.Body>
+                    <Card.Body data-testid="profileInfoWelcome">Your personal data</Card.Body>
                 </Accordion.Collapse>
             </Card>
 
-            <Card className="cardWelcome" data-testid="friendsOptions">
+            <Card className="cardWelcome">
                 <CustomToggle eventKey={5}>
                     <PeopleAltOutlinedIcon className="iconWelcome" data-testid="iconFriendsWelcome" />
-                    <div className="optionTitle">Friends</div>
+                    <div className="optionTitle" data-testid="friendsOptions">Friends</div>
                 </CustomToggle>
                 <Accordion.Collapse eventKey={5} className="mostrar">
                     <Card.Body>
 
-                        <Card className="cardWelcome" data-testid="friends">
+                        <Card className="cardWelcome">
                             <CustomToggle eventKey={5}>
                                 <PeopleAltOutlinedIcon className="iconWelcome" data-testid="iconMyFriendsWelcome" />
-                                <div className="optionTitle">My friends</div>
+                                <div className="optionTitle" data-testid="friends">My friends</div>
                             </CustomToggle>
                             <Accordion.Collapse eventKey={5} className="mostrar">
-                                <Card.Body>Your friends management</Card.Body>
+                                <Card.Body data-testid="friendsInfoWelcome">Your friends management</Card.Body>
                             </Accordion.Collapse>
                         </Card>
 
-                        <Card className="cardWelcome" data-testid="friendsRoutes">
+                        <Card className="cardWelcome">
                             <CustomToggle eventKey={5}>
                                 <PeopleAltOutlinedIcon className="iconWelcome" data-testid="iconFriendsRoutesWelcome" />
-                                <div className="optionTitle">Friend's routes</div>
+                                <div className="optionTitle" data-testid="friendsRoutes">Friend's routes</div>
                             </CustomToggle>
                             <Accordion.Collapse eventKey={5} className="mostrar">
-                                <Card.Body>You can see routes that have been shared to you</Card.Body>
+                                <Card.Body data-testid="friendsRoutesInfoWelcome">You can see routes that have been shared to you</Card.Body>
                             </Accordion.Collapse>
                         </Card>
                     </Card.Body>
                 </Accordion.Collapse>
             </Card>
 
-            <Card className="cardWelcome" data-testid="notifications">
+            <Card className="cardWelcome">
                 <CustomToggle eventKey={6}>
                     <NotificationsActiveOutlinedIcon className="iconWelcome" data-testid="notificationsWelcome" />
-                    <div className="optionTitle">Notifications</div>
+                    <div className="optionTitle" data-testid="notifications">Notifications</div>
                 </CustomToggle>
                 <Accordion.Collapse eventKey={6} className="mostrar">
-                    <Card.Body>You can see notifications related to your profile</Card.Body>
+                    <Card.Body data-testid="notificationsInfoWelcome">You can see notifications related to your profile</Card.Body>
                 </Accordion.Collapse>
             </Card>
 
--- src/tests/Welcome.test.js
+++ src/tests/Welcome.test.js
@@ -22,24 +22,54 @@ test("Render component not crashing.", () => {
 
     expect(getByTestId("add")).toBeInTheDocument();
     expect(getByTestId("add")).toHaveTextContent("Add route");
+
+    expect(getByTestId("addRouteInfoWelcome")).toBeInTheDocument();
+    expect(getByTestId("addRouteInfoWelcome")).toHaveTextContent("You can upload a route and it will be saved in your SOLID pod");
     
     expect(getByTestId("load")).toBeInTheDocument();
-    expect(getByTestId("load")).toHaveTextContent("Load route");
+    expect(getByTestId("load")).toHaveTextContent("My routes");
+
+    expect(getByTestId("loadRouteInfoWelcome")).toBeInTheDocument();
+    expect(getByTestId("loadRouteInfoWelcome")).toHaveTextContent("You can choose one route and show it in the map");
+
+    expect(getByTestId("createRouteInfoWelcome")).toBeInTheDocument();
+    expect(getByTestId("createRouteInfoWelcome")).toHaveTextContent("You can create your own route and save it");
 
     expect(getByTestId("share")).toBeInTheDocument();
     expect(getByTestId("share")).toHaveTextContent("Share route");
 
+    expect(getByTestId("shareRouteInfoWelcome")).toBeInTheDocument();
+    expect(getByTestId("shareRouteInfoWelcome")).toHaveTextContent("You can share your routes with your friends");
+
     expect(getByTestId("profile")).toBeInTheDocument();
     expect(getByTestId("profile")).toHaveTextContent("Profile");
 
+    expect(getByTestId("profileInfoWelcome")).toBeInTheDocument();
+    expect(getByTestId("profileInfoWelcome")).toHaveTextContent("Your personal data");
+
+    expect(getByTestId("friendsOptions")).toBeInTheDocument();
+    expect(getByTestId("friendsOptions")).toHaveTextContent("Friends");
+
+    expect(getByTestId("friends")).toBeInTheDocument();
+    expect(getByTestId("friends")).toHaveTextContent("My friends");
+
+    expect(getByTestId("friendsInfoWelcome")).toBeInTheDocument();
+    expect(getByTestId("friendsInfoWelcome")).toHaveTextContent("Your friends management");
+
     expect(getByTestId("friendsRoutes")).toBeInTheDocument();
     expect(getByTestId("friendsRoutes")).toHaveTextContent("Friend's routes");
-   
-    expect(getByTestId("friends")).toBeInTheDocument();
-    expect(getByTestId("friends")).toHaveTextContent("Friends");
+
+    expect(getByTestId("friendsRoutesInfoWelcome")).toBeInTheDocument();
+    expect(getByTestId("friendsRoutesInfoWelcome")).toHaveTextContent("You can see routes that have been shared to you");
+
+    expect(getByTestId("notifications")).toBeInTheDocument();
+    expect(getByTestId("notifications")).toHaveTextContent("Notifications");
+
+    expect(getByTestId("notificationsInfoWelcome")).toBeInTheDocument();
+    expect(getByTestId("notificationsInfoWelcome")).toHaveTextContent("You can see notifications related to your profile");
 
     expect(getByTestId("message")).toBeInTheDocument();
-    expect(getByTestId("message")).toHaveTextContent("Enjoy our app ! :)");
+    expect(getByTestId("message")).toHaveTextContent("Enjoy our app !");
 
 });
 
