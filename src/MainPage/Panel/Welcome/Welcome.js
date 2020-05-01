4a90251289c1491a52b7874bcf3805246cf7aa4e
Andrea López Suárez <uo264703@uniovi.es>
2020-05-01 14:49:09 +0200

    Add toggle icon. First needs to fix

--- src/MainPage/Panel/Welcome/Welcome.js
+++ src/MainPage/Panel/Welcome/Welcome.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./Welcome.css";
-import Accordion from "react-bootstrap/Accordion";
+import { Nav, Accordion, useAccordionToggle } from 'react-bootstrap'
 import Card from "react-bootstrap/Card";
 import Alert from "react-bootstrap/Button";
 
@@ -20,144 +20,168 @@ import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 
 
 const AccordionWelcome = () => {
-    return (
-        <div className="scrollBarWelcome">
-            <Accordion>
-                <Card className="cardWelcome" data-testid="add">
-                    <Card.Header>
-                        <Accordion.Toggle as={Card.Header} eventKey="0" className="opcionYLogoWelcome">
-                            <BackupOutlinedIcon className="iconWelcome" data-testid="iconAddRouteWelcome" />
-                            <div className="optionTitle">Add route</div>
-                        </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="0" className="mostrar">
-                        <Card.Body>You can upload a route and it will be saved in your SOLID pod</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
 
-                <Card className="cardWelcome" data-testid="routes">
-                    <Card.Header>
-                        <Accordion.Toggle as={Card.Header} eventKey="1" className="opcionYLogoWelcome">
-                            <MapOutlinedIcon className="iconWelcome" data-testid="iconRoutesWelcome" />
-                            <div className="optionTitle">Routes</div>
-                        </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="1" className="mostrar">
-                        <Card.Body>
-                            <Card className="cardWelcome" data-testid="load">
-                                <Card.Header>
-                                    <Accordion.Toggle as={Card.Header} eventKey="8" className="opcionYLogoWelcome">
-                                        <RoomOutlinedIcon className="iconWelcome" data-testid="iconMyRoutesWelcome" />
-                                        <div className="optionTitle">My routes</div>
-                                    </Accordion.Toggle>
-                                </Card.Header>
-                                <Accordion.Collapse eventKey="1" className="mostrar">
-                                    <Card.Body>You can choose one route and show it in the map</Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
-                            <Card className="cardWelcome" data-testid="create">
-                                <Card.Header>
-                                    <Accordion.Toggle as={Card.Header} eventKey="1" className="opcionYLogoWelcome">
-                                        <CreateOutlinedIcon className="iconWelcome" data-testid="iconCreateRouteWelcome" />
-                                        <div className="optionTitle">Create route</div>
-                                    </Accordion.Toggle>
-                                </Card.Header>
-                                <Accordion.Collapse eventKey="1" className="mostrar">
-                                    <Card.Body>You can create your own route and save it</Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
-                        </Card.Body>
-                    </Accordion.Collapse>
-                </Card>
+    const [panelIndex, setPanelIndex] = useState();
 
-                <Card className="cardWelcome" data-testid="share">
-                    <Card.Header>
-                        <Accordion.Toggle as={Card.Header} eventKey="2" className="opcionYLogoWelcome">
-                            <ShareIcon className="iconWelcome" data-testid="iconShareRoutesWelcome" />
-                            <div className="optionTitle">Share routes</div>
-                        </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="2" className="mostrar">
-                        <Card.Body>You can share your routes with your friends</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
+    const CustomToggle = ({ children, eventKey }) => {
 
-                <Card className="cardWelcome" data-testid="profile">
-                    <Card.Header>
-                        <Accordion.Toggle as={Card.Header} eventKey="3" className="opcionYLogoWelcome">
-                            <AccountCircleOutlinedIcon className="iconWelcome" data-testid="iconProfileWelcome" />
-                            <div className="optionTitle">Profile</div>
-                        </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="3" className="mostrar">
-                        <Card.Body>Your personal data</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
+        // Decorate the click event to also set state in the component so we can 
+        // compare the open panel key with the state and toggle classes accordingly
+        const customOnClick = useAccordionToggle(eventKey, () => {
+            setPanelIndex(eventKey === panelIndex ? null : eventKey)
+        })
 
-                <Card className="cardWelcome" data-testid="friendsOptions">
-                    <Card.Header>
-                        <Accordion.Toggle as={Card.Header} eventKey="4" className="opcionYLogoWelcome">
-                            <PeopleAltOutlinedIcon className="iconWelcome" data-testid="iconFriendsWelcome" />
-                            <div className="optionTitle">Friends</div>
-                        </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="4" className="mostrar">
-                        <Card.Body>
-                            <Card className="cardWelcome" data-testid="friends">
-                                <Card.Header>
-                                    <Accordion.Toggle as={Card.Header} eventKey="4" className="opcionYLogoWelcome">
-                                        <PeopleAltOutlinedIcon className="iconWelcome" data-testid="iconMyFriendsWelcome" />
-                                        <div className="optionTitle">My friends</div>
-                                    </Accordion.Toggle>
-                                </Card.Header>
-                                <Accordion.Collapse eventKey="4" className="mostrar">
-                                    <Card.Body>Your friends management</Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
-                            <Card className="cardWelcome" data-testid="friendsRoutes">
-                                <Card.Header>
-                                    <Accordion.Toggle as={Card.Header} eventKey="4" className="opcionYLogoWelcome">
-                                        <PeopleAltOutlinedIcon className="iconWelcome" data-testid="iconFriendsRoutesWelcome" />
-                                        <div className="optionTitle">Friend's routes</div>
-                                    </Accordion.Toggle>
-                                </Card.Header>
-                                <Accordion.Collapse eventKey="4" className="mostrar">
-                                    <Card.Body>You can see routes that have been shared to you</Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
-                        </Card.Body>
-                    </Accordion.Collapse>
-                </Card>
+        const customClass = (eventKey === panelIndex) ? "open-class" : "closed-class"
 
-                <Card className="cardWelcome" data-testid="notifications">
-                    <Card.Header>
-                        <Accordion.Toggle as={Card.Header} eventKey="5" className="opcionYLogoWelcome">
-                            <NotificationsActiveOutlinedIcon className="iconWelcome" data-testid="notificationsWelcome" />
-                            <div className="optionTitle">Notifications</div>
-                        </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="5" className="mostrar">
-                        <Card.Body>You can see notifications related to your profile</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-            </Accordion>
-        </div>
-    )
-}
+        return (
+            <Card.Header>
+                <Accordion.Toggle as={Card.Header} className={customClass} id="opcionYLogoWelcome" onClick={customOnClick}>
+                    {children}
+                </Accordion.Toggle>
+            </Card.Header>
+        )
+    }
 
-const WhatIsViade = () => {
     return (
         <Accordion>
+            <Card className="cardWelcome" data-testid="add">
+                <CustomToggle eventKey={1}>
+                    <BackupOutlinedIcon className="iconWelcome" data-testid="iconAddRouteWelcome" />
+                    <div className="optionTitle">Add route</div>
+                </CustomToggle>
+                <Accordion.Collapse eventKey={1} className="mostrar">
+                    <Card.Body>You can upload a route and it will be saved in your SOLID pod</Card.Body>
+                </Accordion.Collapse>
+            </Card>
+
+            <Card className="cardWelcome" data-testid="routes">
+                <CustomToggle eventKey={2}>
+                    <MapOutlinedIcon className="iconWelcome" data-testid="iconRoutesWelcome" />
+                    <div className="optionTitle">Routes</div>
+                </CustomToggle>
+                <Accordion.Collapse eventKey={2} className="mostrar">
+                    <Card.Body>
+
+                        <Card className="cardWelcome" data-testid="load">
+                            <CustomToggle eventKey={2}>
+                                <RoomOutlinedIcon className="iconWelcome" data-testid="iconMyRoutesWelcome" />
+                                <div className="optionTitle">My routes</div>
+                            </CustomToggle>
+                            <Accordion.Collapse eventKey={2} className="mostrar">
+                                <Card.Body>You can choose one route and show it in the map</Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+
+                        <Card className="cardWelcome" data-testid="create">
+                            <CustomToggle eventKey={2}>
+                                <CreateOutlinedIcon className="iconWelcome" data-testid="iconCreateRouteWelcome" />
+                                <div className="optionTitle">Create route</div>
+                            </CustomToggle>
+                            <Accordion.Collapse eventKey={2} className="mostrar">
+                                <Card.Body>You can create your own route and save it</Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+
+            <Card className="cardWelcome" data-testid="share">
+                <CustomToggle eventKey={3}>
+                    <ShareIcon className="iconWelcome" data-testid="iconShareRoutesWelcome" />
+                    <div className="optionTitle">Share routes</div>
+                </CustomToggle>
+                <Accordion.Collapse eventKey={3} className="mostrar">
+                    <Card.Body>You can share your routes with your friends</Card.Body>
+                </Accordion.Collapse>
+            </Card>
+
+            <Card className="cardWelcome" data-testid="profile">
+                <CustomToggle eventKey={4}>
+                    <AccountCircleOutlinedIcon className="iconWelcome" data-testid="iconProfileWelcome" />
+                    <div className="optionTitle">Profile</div>
+                </CustomToggle>
+                <Accordion.Collapse eventKey={4} className="mostrar">
+                    <Card.Body>Your personal data</Card.Body>
+                </Accordion.Collapse>
+            </Card>
+
+            <Card className="cardWelcome" data-testid="friendsOptions">
+                <CustomToggle eventKey={5}>
+                    <PeopleAltOutlinedIcon className="iconWelcome" data-testid="iconFriendsWelcome" />
+                    <div className="optionTitle">Friends</div>
+                </CustomToggle>
+                <Accordion.Collapse eventKey={5} className="mostrar">
+                    <Card.Body>
+
+                        <Card className="cardWelcome" data-testid="friends">
+                            <CustomToggle eventKey={5}>
+                                <PeopleAltOutlinedIcon className="iconWelcome" data-testid="iconMyFriendsWelcome" />
+                                <div className="optionTitle">My friends</div>
+                            </CustomToggle>
+                            <Accordion.Collapse eventKey={5} className="mostrar">
+                                <Card.Body>Your friends management</Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+
+                        <Card className="cardWelcome" data-testid="friendsRoutes">
+                            <CustomToggle eventKey={5}>
+                                <PeopleAltOutlinedIcon className="iconWelcome" data-testid="iconFriendsRoutesWelcome" />
+                                <div className="optionTitle">Friend's routes</div>
+                            </CustomToggle>
+                            <Accordion.Collapse eventKey={5} className="mostrar">
+                                <Card.Body>You can see routes that have been shared to you</Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+
+            <Card className="cardWelcome" data-testid="notifications">
+                <CustomToggle eventKey={6}>
+                    <NotificationsActiveOutlinedIcon className="iconWelcome" data-testid="notificationsWelcome" />
+                    <div className="optionTitle">Notifications</div>
+                </CustomToggle>
+                <Accordion.Collapse eventKey={6} className="mostrar">
+                    <Card.Body>You can see notifications related to your profile</Card.Body>
+                </Accordion.Collapse>
+            </Card>
+
+        </Accordion>
+    )
+};
+
+const WhatIsViade = () => {
+    const [panelIndex, setPanelIndex] = useState();
+
+    const CustomToggle = ({ children, eventKey }) => {
+
+        // Decorate the click event to also set state in the component so we can 
+        // compare the open panel key with the state and toggle classes accordingly
+        const customOnClick = useAccordionToggle(eventKey, () => {
+            setPanelIndex(eventKey === panelIndex ? null : eventKey)
+        })
+
+        const customClass = (eventKey === panelIndex) ? "open-class" : "closed-class"
+
+        return (
+            <Card.Header>
+                <Accordion.Toggle as={Card.Header} className={customClass} id="opcionYLogoWelcomeViade" onClick={customOnClick}>
+                    {children}
+                </Accordion.Toggle>
+            </Card.Header>
+        )
+    }
+
+    return (
+        <Accordion defaultActiveKey="6">
             <Card className="cardWelcomeInfo" data-testid="introduction">
-                <Card.Header>
-                    <Accordion.Toggle as={Card.Header} eventKey="6" className="opcionYLogoWelcomeViade">
-                        <div className="opcionYLogoWelcomeNearMe">
-                            <div className="optionTitleInfo">What is VIADE?</div>
-                            <NearMeIcon className="iconNearWelcome" data-testid="iconLogout" />
-                        </div>
-                    </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="6" className="mostrarInfo">
+                <CustomToggle eventKey={0}>
+                    <div className="opcionYLogoWelcomeNearMe">
+                        <div className="optionTitleInfo">What is VIADE?</div>
+                        <NearMeIcon className="iconNearWelcome" data-testid="iconLogout" />
+                    </div>
+                </CustomToggle>
+                <Accordion.Collapse eventKey={0} className="mostrarInfo">
                     <Card.Body>Viade is a decentralized routes management system based on the solid specifications.</Card.Body>
                 </Accordion.Collapse>
             </Card>
@@ -175,7 +199,7 @@ const Welcome = () => {
 
             <AccordionWelcome />
             <p data-testid="message">Enjoy our app !
-            <span className="smileFace"><InsertEmoticonIcon className="iconHappy"/></span>
+            <span className="smileFace"><InsertEmoticonIcon className="iconHappy" /></span>
             </p>
 
             <Alert variant={'danger'}>
