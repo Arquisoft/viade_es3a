af1cc8abdaf6cd45f34e8e296edff1ab74e39a2b
christiangrs <christiangrs1998@gmail.com>
2020-05-02 11:44:34 +0200

    show markers

--- src/MainPage/Panel/CreateRoute/CreateRoute.js
+++ src/MainPage/Panel/CreateRoute/CreateRoute.js
@@ -10,6 +10,14 @@ import ReactDOM from "react-dom"
 import UploadRouteToPod from "./UploadRouteToPod";
 import { Loading } from "../../../Loading";
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
 class CreateRoute extends React.Component {
 
     constructor() {
