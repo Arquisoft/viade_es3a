9c1c7c8f7aa7cc2229055a929334e5caa3c6f066
Christian Gabriel <46664254+christiangrs@users.noreply.github.com>
2020-05-03 16:33:03 +0200

    Update Panel.js

--- src/MainPage/Panel/Panel.js
+++ src/MainPage/Panel/Panel.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Panel.css";
 import styled from "styled-components";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import AddRoutePanel from "./AddRoute/AddRoutePanel";
 import LoadRoutePanel from "./LoadRoute/LoadRoutePanel";
 import CreateRoutePanel from "./CreateRoute/CreateRoutePanel";
